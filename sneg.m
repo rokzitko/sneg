c58a942a3fd0be0eaf6bee0536bd98ba19e9d42f
Rok Zitko <rok.zitko@ijs.si>
2025-10-08 17:51:53 +0200

    increaser version number

--- sneg.m
+++ sneg.m
@@ -31,7 +31,7 @@
 
 BeginPackage["Sneg`"];
 
-snegidstring = "sneg.m 2.0.11 Jun 2025";
+snegidstring = "sneg.m 2.0.12 Oct 2025";
 snegcopyright = "Copyright (C) 2002-2025 Rok Zitko";
 
 $SnegVersion = Module[{pos, p1, p2},
